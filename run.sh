992584f0c1a741df259f98b103a67386da3a927a
Yujin Ariza <yujin.ariza@gmail.com>
2014-12-14 23:10:49 -0500

    script

---
+++ run.sh
@@ -0,0 +1,21 @@
+mkdir process/build;
+cd process/build; cmake .. && make ;
+
+cd -;
+
+mkdir render/build;
+cd render/build; cmake .. && make ;
+
+cd -;
+
+for name in "$@"; do
+
+    cd process/build;
+    ./process -i "assets/imagesin/"$name".png" -s "assets/styles/"$name".yaml" -o "assets/canvas/"$name".yaml";
+
+    cd -;
+
+    cd render/build;
+    ./render "assets/canvas/"$name".yaml" -s "assets/styles/"$name".yaml" -o "results/"$name".png";
+
+done
