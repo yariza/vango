76b45cdfeae129f4ac5f50a1e6badc506fb4452d
Yujin Ariza <yujin.ariza@gmail.com>
2014-12-14 23:30:18 -0500

    fixed script

--- run.sh
+++ run.sh
@@ -10,12 +10,16 @@ cd -;
 
 for name in "$@"; do
 
+    echo;
+    echo $name;
+
     cd process/build;
     ./process -i "assets/imagesin/"$name".png" -s "assets/styles/"$name".yaml" -o "assets/canvas/"$name".yaml";
 
     cd -;
 
     cd render/build;
-    ./render "assets/canvas/"$name".yaml" -s "assets/styles/"$name".yaml" -o "results/"$name".png";
+    ./render "assets/canvas/"$name".yaml" -s "assets/styles/"$name".yaml" -o "../../results/"$name".png" -d 0;
 
+    cd -;
 done
