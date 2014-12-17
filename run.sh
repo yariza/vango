4907039af66f56fc61a7c1d18c0f826fc6dcfe9c
Yujin Ariza <yujin.ariza@gmail.com>
2014-12-17 13:53:54 -0500

    modified run script

--- run.sh
+++ run.sh
@@ -14,12 +14,12 @@ for name in "$@"; do
     echo $name;
 
     cd process/build;
-    ./process -i "assets/imagesin/"$name".png" -s "assets/styles/"$name".yaml" -o "assets/canvas/"$name".yaml";
+    ./process -i "../../assets/imagesin/"$name".png" -s "../../assets/styles/"$name".yaml" -o "../../assets/canvas/"$name".yaml";
 
     cd -;
 
     cd render/build;
-    ./render "assets/canvas/"$name".yaml" -s "assets/styles/"$name".yaml" -o "../../results/"$name".png" -d 0;
+    ./render "../../assets/canvas/"$name".yaml" -s "../../assets/styles/"$name".yaml" -o "../../results/"$name".png" -d 0;
 
     cd -;
 done
